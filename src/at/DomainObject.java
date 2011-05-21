47cc2dccfc46aef3b28311da45d67b32c0d9f397
Artur <steffi.eibensteiner@gmx.at>
2011-05-21 12:29:22 +0200

    <Tag 5> Artur: "DomainObject.java: getter und setter für name-Attribut"

--- src/at/DomainObject.java
+++ src/at/DomainObject.java
@@ -36,5 +36,13 @@ public void setComment(String comment) {
    this.comment = comment;
 }
 
+public String getName() {
+   return name;
+}
+
+public void setName(String name) {
+   this.name = name;
+}
+
 
 }
