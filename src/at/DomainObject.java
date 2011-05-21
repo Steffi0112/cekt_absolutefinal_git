f6f4ba1a749a263f3146be1887760f2132d17f26
Barbara <emir.handukic@gmx.at>
2011-05-21 13:08:01 +0200

    <Tag 9> Barbara: "DomainObject: hashCode() Methode überschrieben"

--- src/at/DomainObject.java
+++ src/at/DomainObject.java
@@ -16,6 +16,14 @@ public String toString() {
 }
 
 
+public int hashCode() {
+   if (uuid != null) {
+      return uuid.hashCode();
+   } else {
+      return super.hashCode();
+   }
+}
+
 public DomainObject(String name, String comment)  {
   super();
   this.id = id;
