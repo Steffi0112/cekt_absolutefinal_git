d90101ba409959e546b716dd568223fcd4986377
Clemens <daniela.stoiber@gemserver.at>
2011-05-21 11:49:55 +0200

    <Tag 5> Clemens: "Main.java (Release 1.0): Kommentar als zweites Konstruktorelement hinzufügen"

--- src/at/Main.java
+++ src/at/Main.java
@@ -11,11 +11,11 @@ private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 Set<DomainObject> objectSet = new HashSet<DomainObject>();
 
-objectSet.add(new DomainObject("Helga"));
-objectSet.add(new DomainObject("Walter"));
-objectSet.add(new DomainObject("Hannah"));
-objectSet.add(new DomainObject("Paul"));
-objectSet.add(new DomainObject("Claudia"));
+objectSet.add(new DomainObject("Helga", "Kommentar"));
+objectSet.add(new DomainObject("Walter", "Kommentar"));
+objectSet.add(new DomainObject("Hannah", "Kommentar"));
+objectSet.add(new DomainObject("Paul", "Kommentar"));
+objectSet.add(new DomainObject("Claudia", "Kommentar"));
 
 for (DomainObject domainObject : objectSet) {
   LOGGER.debug(domainObject.id);
